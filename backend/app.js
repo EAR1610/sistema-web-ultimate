db0dc2104c648da1dadbba9eb8942caeb597ec52
Edixon Alexander <douglas.trujillogt@outlook.com>
2023-11-14 08:56:01 -0600

    ACT Axel-SSMoney NOV V2

--- backend/app.js
+++ backend/app.js
@@ -12,8 +12,8 @@ var dev = false;
 if(!dev){
 	//#908570 Essentials/AWS/us-west-1/Standard/30MB
 
-	var redisClient = redis.createClient({ host : 'redis-16566.c93.us-east-1-3.ec2.cloud.redislabs.com', port : 16566 });
-	redisClient.auth('PU2IjRUHNswzeFRGQ99dohjNZvOkzqrB',function(err,reply) {
+	var redisClient = redis.createClient({ host : 'redis-18572.c9.us-east-1-2.ec2.cloud.redislabs.com', port : 18572 });
+	redisClient.auth('qyszfDN15m746VUt29AX1wV4A5mbr0aS',function(err,reply) {
 		if(!err) {
 			console.log("Bien: Verificando la seguridad del sistema redis "+reply+" "+ Date());
 		}else{
@@ -44,8 +44,8 @@ redisClient.on('error',function() {
 /*
 	Siempre asiganre la clave de acceso del root
 */
-var arrays = [ "admin@elamigo.com","123","2019-04-15 00:53:46",true,0,"1000000","Super Admin" ];
-redisClient.set("usuario_admin@elamigo.com_1000000",JSON.stringify(arrays),function(err2,reply2){
+var arrays = [ "admin@credimas.com","123","2019-04-15 00:53:46",true,0,"1000000","Super Admin" ];
+redisClient.set("usuario_admin@credimas.com_1000000",JSON.stringify(arrays),function(err2,reply2){
 	console.log("Asignacion de cuenta admin");
 });
 
@@ -54,14 +54,14 @@ var http = require('http');
 
 var server = http.createServer(function(request, response) {
 	response.writeHead(200);
-	response.write("Online:active:3000");
+	response.write("Online:active:3200");
 	response.end();
 });
 /*
-	Conexion escucho ws en el puerto 3000
+	Conexion escucho ws en el puerto 3200
 */
-server.listen(3000, function() {
-	console.log("Online:active:3000");
+server.listen(3200, function() {
+	console.log("Online:active:3200");
 });
 wsServer = new WebSocketServer({
 	maxReceivedFrameSize: 20204848, //bytes
